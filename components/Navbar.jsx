2bf7032f3b5a619f9492f40d640f59eaec95a121
Nina Okumura <elaineokumura@gmail.com>
2022-11-01 20:31:09 +1300

    implement mobile nav menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Image from "next/image";
-// import close from "../public/assets/shared/icon-close.svg";
+import close from "../public/assets/shared/icon-close.svg";
 import menu from "../public/assets/shared/icon-hamburger.svg";
+import clsx from "clsx";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -26,19 +27,32 @@ export default function Navbar() {
   ];
 
   return (
-    <div className="text-white">
-      <div className="md:hidden">
-        <Image src={menu} />
-      </div>
-
-      <ul className="gap-4 hidden md:flex">
+    <div>
+      <ul
+        className={clsx(
+          "gap-12 md:flex absolute bottom-0 h-screen top-0 right-0 md:py-12 md:pr-28",
+          {
+            "hidden md:flex": !isMenuOpen,
+            "backdrop-blur-xl opacity-100 py-28 px-8 w-[254px]": isMenuOpen,
+          }
+        )}
+      >
         {links.map(link => (
-          <li key={link.num}>
-            <span className="md:hidden">{link.num}</span>
-            <span>{link.label}</span>
+          <li
+            key={link.num}
+            className="flex gap-4 font-barlow pb-8 text-light text-base tracking-[2.7px]"
+          >
+            <span className="md:hidden lg:flex font-semibold">{link.num}</span>
+            <span>{link.label.toUpperCase()}</span>
           </li>
         ))}
       </ul>
+
+      <div className="md:hidden">
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          {isMenuOpen ? <Image src={close} /> : <Image src={menu} />}
+        </button>
+      </div>
     </div>
   );
 }
